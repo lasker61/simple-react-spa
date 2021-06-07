fdcd2914909fd310d89e9fd15f56da35ec286bb4
lasker61 <laskernahid2@gmail.com>
2021-06-07 22:49:16 +0600

    updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 // import { useState } from 'react';
@@ -10,6 +10,20 @@ const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
+
+    useEffect(()=>{
+        const savedCart =getDatabaseCart();
+        const productKeys =Object.keys(savedCart);
+        const previousCart =productKeys.map(existingKey =>{
+            const product = fakeData.find(pd =>pd.key === existingKey);
+            product.quantity=savedCart[existingKey];
+            return product;
+        })
+        setCart(previousCart);
+        console.log(previousCart);
+
+    },[])
+
     const handleAddProduct = (product) => {
         // console.log('product added',product);
         const toBeAddededKey =product.key;
